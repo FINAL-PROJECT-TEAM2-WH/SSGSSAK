5424a7d9bbbc5df2f330bf9fec03beab7b0c49a9
daetu01 <daetu01@gachon.ac.kr>
2024-06-01 10:27:55 +0900

    utf-8 update

--- src/main/java/ssgssak/team1/sist/service/member/LoginService.java
+++ src/main/java/ssgssak/team1/sist/service/member/LoginService.java
@@ -2,10 +2,8 @@ package ssgssak.team1.sist.service.member;
 
 import org.springframework.stereotype.Service;
 
-@Service
 public interface LoginService {
-	public LoginServiceImpl lao = null;
-	
+
 	public boolean login(String id, String passwd);	
 	
 	public int idInvalid(String id);
